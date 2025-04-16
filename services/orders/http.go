438354dc854d57d7958aabaa3935b68c2c84555a
Adam Mwaniki <adamndegwa@protonmail.com>
2025-04-16 21:24:53 +0300

    Implements http server

--- services/orders/http.go
+++ services/orders/http.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/adammwaniki/kitchen-micro-service/services/orders/service"
+)
+
+// Implementations will be similar to grpc
+type httpServer struct {
+	addr string
+}
+
+func NewHttpServer(addr string) *httpServer {
+	return &httpServer{addr: addr}
+}
+
+func (s *httpServer) Run() error {
+	router := http.NewServeMux()
+
+	orderService := service.NewOrderService() // Implementing the orderService this way allows separation of concerns from business logic in the services
+
+	log.Println("Starting server on", s.addr)
+
+	return http.ListenAndServe(s.addr, router)
+}
